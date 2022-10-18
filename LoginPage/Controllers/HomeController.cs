77b58a43e87e6729ed9e27153fbb9f2f0da84ea1
STS665-VIMALA S <vimalasivashanmugam1@gmail.com>
2022-10-18 15:46:51 +0530

    Add project files.

---
+++ LoginPage/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            ViewBag.area = "Hii";
+            ViewData["area"] = "Welcome";
+            TempData["area"] = "Have a nice day";
+            
+            return View();
+        }
+        public ActionResult Correct(Sample set)
+        {
+            if (set.Name == "Vimala" && set.Password == 12345)
+            {
+                ViewBag.name = "Success";
+                ViewData["Data"] = "Done";
+                TempData["value"] = "Welcome";
+                return View();
+            }
+            else
+            {
+                ViewBag.name = "Sorry";
+                ViewData["data"] = "Error";
+                TempData["value"] = "Please Check the name and password";
+                return View();
+            }
+        }
+
+               
+ 
+    }
+}
